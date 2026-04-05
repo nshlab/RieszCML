76c33db777a21b21d75e38742e3bb80e9b0a8328
Christian Testa <ctesta@hsph.harvard.edu>
2026-04-05 11:16:11 -0400

    Fix error in ATE and ATT

--- R/catalog.R
+++ R/catalog.R
@@ -70,26 +70,38 @@ riesz_curve_catalog$cfmean_a0 <- function(nuis) {
   riesz_curve_catalog$cfmean_a(a = 0, nuis = nuis)
 }
 
-
 riesz_curve_catalog$ate <- function(nuis) {
-
   RieszCurve$new(
     nuis = nuis,
-    alpha = ~ A/g + (1 - A)/(1 - g),
+    alpha = ~ A/g - (1 - A)/(1 - g),
     f = ~ m,
     h = ~ m1 - m0,
     ic_expr = ~ h + alpha * (Y - f)
   )
 }
 
+#'
+#' @examples
+#' # nuisances needed for att:
+#' nuis_att <- function(data) {
+#' g_fit  <- nadir::lnr_logistic(data, A ~ L1 + L2 + L3)
+#' m0_fit <- nadir::lnr_glm(subset(data, A == 0), Y ~ L1 + L2 + L3)
+#'
+#' list(
+#'   g  = g_fit(data),
+#'   m0 = m0_fit(data),
+#'   p  = mean(data$A)
+#' )
+#' }
+#'
 riesz_curve_catalog$att <- function(nuis) {
 
   RieszCurve$new(
     nuis = nuis,
-    alpha = ~ A + (A - g)/(g * (1 - g)),
-    f = ~ m,
-    h = ~ m1 - m0,
-    ic_expr = ~ A * (Y - m0) / mean(A) + (m1 - m0) - h
+    alpha = ~ A / p - (1 - A) * g / (p * (1 - g)),
+    f = ~ m0,
+    h = ~ A * (Y - m0) / p,
+    ic_expr = ~ h + alpha * (Y - f)
   )
 }
 
